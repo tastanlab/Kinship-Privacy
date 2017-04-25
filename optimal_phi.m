c2ee10a0b90a4fb0773a3cec455fdf0eb7906b87
gulcek <gulce.kale@gmail.com>
2017-04-25 20:35:57 +0300

    Create optimal_phi.m

---
+++ optimal_phi.m
@@ -0,0 +1,63 @@
+function [x,fval, exitflag] = ga_lp2()
+
+phi = 0; % it might be higher values i.e. if previous members are added,
+%and the optimal solution is found when phi=0.07, then assign 0.07
+M = dlmread('kin_constraints.txt','', 0,0 )  ;
+s = (size(M,2)-2)/2;
+
+x0 = [ zeros(s+1,1)];
+lb = [zeros(s,1); phi];
+size_constraints = dlmread('size_constraints.txt');
+ub =[size_constraints ; 0.5];
+IntCon=[1:s];
+
+Aeq = [];
+beq = [];
+A = [];
+b = [];
+
+if(size(dir('*.vcf'),1) > 2)
+    [x,fval,exitflag,output]=ga(@myobj,s+1,A,b,Aeq,beq,lb,ub,@myconstr, IntCon);   
+else
+    [x,fval,exitflag,output]=ga(@myobj2,s+1,A,b,Aeq,beq,lb,ub,@myconstr2, IntCon); 
+end
+fval
+fileID = fopen('phi.txt','wt');
+fprintf(fileID, '%f', fval');
+fclose(fileID);
+
+function f = myobj(x)
+M = dlmread('kin_constraints.txt','', 1,0 )  ;
+s = (size(M,2)-2)/2;
+f=x(s+1);
+end
+%---------------------------------------------------------
+function [c, ceq] = myconstr(x)
+M = dlmread('kin_constraints.txt','', 0,0 ) ;
+    s = (size(M,2)-2)/2;
+    r = size(M,1);
+    c = [];
+    %% every constraint
+    for i = 1:r
+        a = 0;
+        for j = 1:s
+            a = a + x(j) * M(i,j);
+            a = a + x(j) * M(i,j+s) * x(s+1);
+        end
+        a = a + x(s+1) * M(i,2*s+2);
+        a = a - M(i,(2 * s + 1));
+        c = [c; -a];
+    end
+    
+    ceq=[];
+end  
+%---------------------------------------------------------
+function f = myobj2(x)
+f=x(2);
+end
+%---------------------------------------------------------    
+function [c, ceq] = myconstr2(x)
+c = [ -M(1)*x(1) - M(2)*x(1)*x(2) + M(4)*x(2) + M(3)];
+ceq = [];
+end
+end
